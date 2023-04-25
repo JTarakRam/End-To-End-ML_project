833359a367ff7515cb0e55d5a855671e5ba65250
Tarak Ram <jujjurutarakram1818@gmail.com>
2023-04-25 16:33:33 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionconfig:
@@ -45,4 +48,8 @@ class DataIngestion :
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()            
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+

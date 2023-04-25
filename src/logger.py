f2075e57e7cc5797a0e86732ef3ea4339084052f
Tarak Ram <jujjurutarakram1818@gmail.com>
2023-04-25 14:47:44 +0530

    init

--- src/logger.py
+++ src/logger.py
@@ -10,7 +10,7 @@ LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename= LOG_FILE_PATH,
     format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO
+    level= logging.INFO,
 
 )
 

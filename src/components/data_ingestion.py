ff83c74cf4496b135280f94359b1f08035f1012c
Tarak Ram <jujjurutarakram1818@gmail.com>
2023-04-25 14:55:36 +0530

    Artifacts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,11 +9,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
 @dataclass
 class DataIngestionconfig:
     train_data_path : str = os.path.join('Artifacts','train.csv')
     test_data_path : str = os.path.join('Artifacts','test.csv')
-    raw_data_path : str = os.path.join('Artifacts','raw_data.csv')
+    raw_data_path : str = os.path.join('Artifacts','data.csv')
 
     
 class DataIngestion :
@@ -26,12 +27,12 @@ class DataIngestion :
             logging.info('Read the dataset !')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
-            df.to_csv(self.ingestion_config.train_data_path, index = False, header= True)
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header= True)
             logging.info('Train test split intiated !')
             train_set, test_set = train_test_split(df, test_size= 0.2, random_state= 42)
-
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
             logging.info('Ingestion of the data is completed !')
 
             return (
